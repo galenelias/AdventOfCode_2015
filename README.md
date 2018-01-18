08ead8ec979469ce5ffe39055ad9688289bf72dd
Galen Elias <gelias@gmail.com>
2018-01-17 22:44:25 -0800

    Initial base check-in

---
+++ README.md
@@ -0,0 +1,7 @@
+# Advent Of Code 2017
+Dumping ground for Advent of Code 2017 solutions in various languages (C++, Rust, F#)
+
+| Problem              | Rust                  |
+|----------------------|-----------------------|
+| [Day 1](src/Day1/)   | [✔](src/Day1/mod.rs) |
+| [Day 2](src/Day2/)   | [✔](src/Day2/mod.rs) |
---
+++ src/main.rs
@@ -0,0 +1,44 @@
+#![feature(slice_rotate)]
+#![feature(iterator_step_by)]
+#![feature(iterator_try_fold)]
+
+extern crate clap;
+extern crate regex;
+
+use clap::{Arg,App};
+
+mod day1;
+
+fn main() {
+
+	let matches = App::new("Advent of Code")
+		.author("Galen Elias, gelias@gmail.com")
+		.version("0.1.0")
+		.about("Advent of code solutions in Rust")
+		.arg(
+			Arg::with_name("day")
+				.short("d")
+				.required(true)
+				.index(1)
+				.help("specifies which day's challenge to run")
+				.validator(|str|
+					str.parse::<u32>()
+						.or(Err("day must be an integer".to_owned()))
+						.and_then(|v| match v {
+							0...25 => Ok(()),
+							_ => Err("day must be between 1 and 25".to_owned())
+						})))
+		.arg(
+			Arg::with_name("stats")
+				.long("stats")
+				.help("Parses leaderboard JSON into a readable format"))
+		.after_help("Longer explaination to appear after the options when \
+					displaying the help information from --help or -h")
+		.get_matches();
+
+	let day = matches.value_of("day").unwrap().parse::<u32>().unwrap();
+	match day {
+		1 => day1::solve(),
+		_ => println!("Oops! Day {} isn't implemented yet!", day)
+	}
+}
