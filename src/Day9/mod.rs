a597c0912b38383cb72978a5be8dd8d4d1d55a53
Galen Elias <gelias@microsoft.com>
2018-05-17 12:22:58 -0700

    Further refine index solution to use 2D distance grid instead of HashSet of index pairs.  Over 10x faster.

--- src/Day9/mod.rs
+++ src/Day9/mod.rs
@@ -3,12 +3,10 @@ use std::collections::{HashMap, HashSet};
 use itertools::Itertools;
 use permutohedron::LexicalPermutation;
 
-fn calc_route_distance(cities: &[usize], distances: &HashMap<(&usize,&usize), u32>) -> u32 {
+fn calc_route_distance(cities: &[usize], distances: &Vec<Vec<u32>>) -> u32 {
 	let mut dist: u32 = 0;
 	for i in 0..cities.len()-1 {
-		// TODO: More efficient lookups. These clones will be terrible.
-		//  HashMap<&str,&str>?
-		dist += distances.get(&(&cities[i], &cities[i+1])).unwrap();
+		dist += distances[cities[i]][cities[i+1]];
 	}
 	return dist;
 }
@@ -21,28 +19,27 @@ pub fn solve() {
 		.collect_vec();
 
 	let mut cities = HashSet::new();
-
 	for line in &lines {
 		cities.insert(line[0].clone());
 		cities.insert(line[2].clone());
 	}
-	let cities_vec = cities.iter().cloned().collect_vec();
+
+	let cities_vec = cities.iter().collect_vec();
 	let mut cities_idx_map = HashMap::new();
 	for (i, val) in cities_vec.iter().enumerate() {
 		cities_idx_map.insert(val.clone(), i);
 	}
 
-	let mut distances = HashMap::new();
+	let mut distances = vec![vec![0; cities_vec.len()]; cities_vec.len()];
 
 	for line in &lines {
 		let city1_ord = cities_idx_map.get(&line[0]).unwrap();
 		let city2_ord = cities_idx_map.get(&line[2]).unwrap();
 		let dist = line[4].parse::<u32>().unwrap();
-		distances.insert((city1_ord, city2_ord), dist);
-		distances.insert((city2_ord, city1_ord), dist);
+		distances[*city1_ord][*city2_ord] = dist;
+		distances[*city2_ord][*city1_ord] = dist;
 	}
 
-	// cities_vec.sort();
 	let mut cities_vec_nums = (0..cities_vec.len()).collect_vec();
 	let mut min_distance: Option<u32> = None;
 	let mut max_distance: Option<u32> = None;
