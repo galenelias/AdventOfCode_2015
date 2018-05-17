4f61004b9c3e857f116941a40cb569614525b39f
Galen Elias <gelias@microsoft.com>
2018-05-17 11:51:12 -0700

    Some perf optimizations. Index by city numbers, not strings

--- src/Day9/mod.rs
+++ src/Day9/mod.rs
@@ -3,12 +3,12 @@ use std::collections::{HashMap, HashSet};
 use itertools::Itertools;
 use permutohedron::LexicalPermutation;
 
-fn calc_route_distance(cities: &[String], distances: &HashMap<(String,String), u32>) -> u32 {
+fn calc_route_distance(cities: &[usize], distances: &HashMap<(&usize,&usize), u32>) -> u32 {
 	let mut dist: u32 = 0;
 	for i in 0..cities.len()-1 {
 		// TODO: More efficient lookups. These clones will be terrible.
 		//  HashMap<&str,&str>?
-		dist += distances.get(&(cities[i].clone(), cities[i+1].clone())).unwrap();
+		dist += distances.get(&(&cities[i], &cities[i+1])).unwrap();
 	}
 	return dist;
 }
@@ -20,29 +20,48 @@ pub fn solve() {
 		.map(|line| line.split_whitespace().map(String::from).collect_vec())
 		.collect_vec();
 
-	let mut distances = HashMap::new();
 	let mut cities = HashSet::new();
 
 	for line in &lines {
-		distances.insert((line[0].clone(), line[2].clone()), line[4].parse::<u32>().unwrap());
-		distances.insert((line[2].clone(), line[0].clone()), line[4].parse::<u32>().unwrap());
 		cities.insert(line[0].clone());
 		cities.insert(line[2].clone());
 	}
+	let cities_vec = cities.iter().cloned().collect_vec();
+	let mut cities_idx_map = HashMap::new();
+	for (i, val) in cities_vec.iter().enumerate() {
+		cities_idx_map.insert(val.clone(), i);
+	}
 
-	let mut cities_vec = cities.iter().cloned().collect_vec();
+	let mut distances = HashMap::new();
+
+	for line in &lines {
+		let city1_ord = cities_idx_map.get(&line[0]).unwrap();
+		let city2_ord = cities_idx_map.get(&line[2]).unwrap();
+		let dist = line[4].parse::<u32>().unwrap();
+		distances.insert((city1_ord, city2_ord), dist);
+		distances.insert((city2_ord, city1_ord), dist);
+	}
+
+	// cities_vec.sort();
+	let mut cities_vec_nums = (0..cities_vec.len()).collect_vec();
 	let mut min_distance: Option<u32> = None;
+	let mut max_distance: Option<u32> = None;
 
 	loop {
-		let dist = calc_route_distance(&cities_vec[..], &distances);
+		let dist = calc_route_distance(&cities_vec_nums[..], &distances);
 		if min_distance.is_none() || dist < min_distance.unwrap() {
 			min_distance = Some(dist);
 		}
 
-		if !cities_vec.next_permutation() {
+		if max_distance.is_none() || dist > max_distance.unwrap() {
+			max_distance = Some(dist);
+		}
+
+		if !cities_vec_nums.next_permutation() {
 			break;
 		}
 	}
 
 	println!("Part 1: {}", min_distance.unwrap_or_default());
+	println!("Part 2: {}", max_distance.unwrap_or_default());
 }
